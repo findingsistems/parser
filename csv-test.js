39b91a992cca8d54304a77384622772894d9f414
witem <witem.artem@gmail.com>
2015-08-12 08:52:10 +0300

    profiling csv converter

--- csv-test.js
+++ csv-test.js
@@ -4,19 +4,26 @@ var fs = require("fs"),
     Transform = require('stream').Transform,
     JSFtp = require("jsftp"),
     async = require("async"),
-    csv = require('csv-streamify-mod');
+    csv = require('csv-parser');
+//var csv_opts = {
+//    delimiter: ',',
+//    newline: '\n',
+//    quote : '\"',
+//    objectMode: true
+//};
 var csv_opts = {
-    delimiter: ',',
+    raw: false,
+    separator: ',',
     newline: '\n',
-    quote : '\"',
+    headers: ['0', '1', '2', '3', '4', '5'],
     objectMode: true
 };
 var transform_cb = function(data, encoding, done) {
     // id | code | relevant_code | manufacturer | name | count | price | price_files_id | user_id | fts | delivere
-    if ((!data[2] || data[2].length > 300) || (!data[3] || data[3].length > 150) || (!data[4] || data[4].length > 150) || (!data[5] || data[5].length > 150)) {
-        console.log('###ALERT###');
-        console.log(data);
-    }
+    //if ((!data[2] || data[2].length > 300) || (!data[3] || data[3].length > 150) || (!data[4] || data[4].length > 150) || (!data[5] || data[5].length > 150)) {
+    //    console.log('###ALERT###');
+    //    console.log(data);
+    //}
     var t = "\",\"";
     if (isNaN(+data[3]))
         return done();
@@ -32,18 +39,72 @@ var transform_cb = function(data, encoding, done) {
     done();
 };
 
-var csvToJson = csv(csv_opts); //todo make better
+var ts_1 = 0,
+    ts_2 = 0,
+    ts_3 = 0,
+    ts_4 = 0,
+    ts_5 = 0,
+    c_o  = 0,
+    c_2  = 0,
+    c_3  = 0,
+    tick = 100000;
+var out = fs.createWriteStream("temp/test-1.csv");
 var parser = new Transform({objectMode: true}); //todo make better
 parser._transform = transform_cb;
-var out = fs.createWriteStream("temp/test-1.csv");
+parser.on("data", function(data){
+    if (c_3%tick === 0) {
+        if (c_3 > 0)
+            console.log('csvToJson', ~~(c_3/tick), Date.now() - ts_3);
+    }
+    c_3++;
+    out.write(data);
+})
+.on('end', function () {
+    out.end();
+});
+var csvToJson = csv(csv_opts); //todo make better
+csvToJson.on("data", function(data){
+    if (c_2%tick === 0) {
+        ts_3 = Date.now();
+        if (c_2 > 0) {
+            console.log('csvToJson', ~~(c_2/tick), Date.now() - ts_2 - ts_4);
+            ts_4 = Date.now() - ts_2;
+        }
+    }
+    c_2++;
+    parser.write(data);
+})
+.on('end', function () {
+    parser.end();
+});
 
+console.log('#start');
+ts_1 = Date.now();
 fs.createReadStream('temp/' + "autogiper_all_15.csv")
-    .pipe(csvToJson)
-    .pipe(parser)
-    .pipe(out)
-    .on('finish', function () {
-        console.log('#finish', new Date());
+    .on('data', function(chunk){
+        if (c_o%tick === 0) {
+            console.log('read 1000')
+            ts_2 = Date.now();
+        }
+        c_o++;
+        csvToJson.write(chunk);
+    })
+    .on('end', function () {
+        csvToJson.end();
     })
     .on('error', function (err) {
-        console.log('error', err);
+        console.log('error ReadStream', err);
     });
+out.on('finish', function(){
+    console.log('#finish', ~~((Date.now() - ts_1)/1000));
+});
+//fs.createReadStream('temp/' + "autogiper_all_15.csv")
+//    .pipe(csvToJson)
+//    .pipe(parser)
+//    .pipe(out)
+//    .on('finish', function () {
+//        console.log('#finish', ~~((Date.now() - ts_1)/1000), new Date());
+//    })
+//    .on('error', function (err) {
+//        console.log('error', err);
+//    });
