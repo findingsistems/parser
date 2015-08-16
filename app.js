c8b7fb9aaa467bcca302436abbd776946db7ddd6
witem <witem.artem@gmail.com>
2015-08-16 22:59:42 +0300

    rafactoring transform cb

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+"use strict";
 process.on('uncaughtException', function (err) {
   console.error(err);
   console.error(err.stack);
@@ -17,8 +18,7 @@ var fs = require("fs"),
   jschardet = require("jschardet"),
   Transform = require('stream').Transform;
 
-var config = {},
-  db_client = null,
+var config, db_client, parse_intv,
   parse_cycle_active = false;
 
 var read_config = function () {
@@ -33,21 +33,7 @@ var read_config = function () {
   //todo check config structure
 };
 read_config();
-//var task_processed = function () {
-//    var task = config.list.shift();
-//    console.log("processed task", task.name, "|", new Date());
-//    switch (task.type) {
-//        case "http":
-//            http_processed(task);
-//            break;
-//        case "ftp":
-//            console.log("ftp");
-//            break;
-//        default:
-//            console.error("Unspecified type:", task.type, "| Task:", task.name);
-//    }
-//};
-//
+
 //var http_processed = function (task) {
 //    var regexp_file_url, regexp_str;
 //    var i = 0;
@@ -144,27 +130,116 @@ read_config();
 //        })(v);
 //    },500);
 //};
-
-var transform_cb = function (data, encoding, done) { //todo configurate from config file
-  if ((!data[2] || data[2].length > 300) || (!data[3] || data[3].length > 150) || (!data[4] || data[4].length > 150) || (!data[5] || data[5].length > 150)) {
-    console.log('###ALERT###');
-    console.log(data);
+/*
+- default value
+- cancel if empty || NaN
+- regexp
+- function
+-- toInt
+ */
+var cancel_check = function( type, data, done ) {
+  var check_ok = true;
+  switch ( type ){
+    case "NaN" :
+      check_ok = isNaN( data );
+      break;
+    case "length" :
+      check_ok = data != null && !data.length;
+      break;
+    case "null" :
+    case "undefined" :
+      check_ok = data != null && data !== undefined;
+      break;
+    default :
+      console.error( "Cancel check", type );
   }
-  var t = "\",\"";
-  if (isNaN(+data[3]))
+  if ( check_ok ){
+    return data;
+  } else {
     return done();
-  var manufacturer = data[0];
-  var code = data[1].replace(/[^a-zA-Z0-9]/gi, "");
-  var name = data[2];
-  var count = ~~data[3];
-  var price = (~~data[4]);
-  var delivere = (data[5] || "").replace(/[^0-9\-]/gi, "");
-  var str = "\"" + manufacturer + t + code + t + name + t + count + t + price + t + delivere + t + this._price_files_id + t + this._user_id + "\"";
-  this.push(str + "\n");
-  done();
+  }
 };
+
+var transform_column = function( column_opt ) {
+  var r;
+  switch ( column_opt.type ) {
+    case "regexp" :
+      r = new RegExp( column_opt.value, column_opt.options );
+      return function( data, done ){
+        var ret = data[column_opt.column].replace( r, "" );
+        if ( column_opt.cancel != null ) {
+          return cancel_check( column_opt.cancel, ret, done );
+        } else {
+          return ret;
+        }
+      };
+      break;
+    case "number_latter":
+      return function( data, done ){
+        var ret = data[column_opt.column].replace( /[^a-zA-Z0-9]/gi, "" );
+        if ( column_opt.cancel != null ) {
+          return cancel_check( column_opt.cancel, ret, done );
+        } else {
+          return ret;
+        }
+      };
+      break;
+    case "to_int":
+      return function( data, done ){
+        var ret = ~~data[column_opt.column];
+        if ( column_opt.cancel != null ) {
+          return cancel_check( column_opt.cancel, ret, done );
+        } else {
+          return ret;
+        }
+      };
+      break;
+    default :
+      if ( !column_opt.type ) {
+        return function( data ){
+          return data[column_opt.column];
+        }
+      } else {
+        console.error( "Not defined type", column_opt.type);
+        return function( data ){
+          return data[column_opt.column];
+        }
+      }
+  }
+
+};
+
+var transform_compiler = function( task ) {
+  var manufacturer = transform_column( task.transform_opts.manufacturer );
+  var code = transform_column( task.transform_opts.code );
+  var name = transform_column( task.transform_opts.name );
+  var count = transform_column( task.transform_opts.count );
+  var price = transform_column( task.transform_opts.price );
+  var delivere = transform_column( task.transform_opts.delivere );
+
+  return function (data, encoding, done) {
+    if ( (!data[2] || data[2].length > 300) || (!data[3] || data[3].length > 150) || (!data[4] || data[4].length > 150) || (!data[5] || data[5].length > 150) ) {
+      console.log('###ALERT###');
+      console.log(data);
+    }
+    var t = "\",\"";
+    var str = "\"" + manufacturer( data, done ) + t ;
+    str += code( data, done ) + t;
+    str += name( data, done ) + t;
+    str += count( data, done ) + t;
+    str += price( data, done ) + t;
+    str += delivere( data, done ) + t;
+    str += this._price_files_id + t + this._user_id + "\"";
+    this.push(str + "\n");
+    done();
+  };
+};
+
 var query = 'COPY prices_wholesale (manufacturer, code, name, count, price, delivere, price_files__id, user__id) FROM STDIN CSV';
 
+/*
+ * PROCESSED FILE
+ */
 var processed_file = async.queue(function (obj, callback) { //todo make better
   var toJSON, parser, stream_db;
   if ( /\.(csv)$/i.test( obj.file_name ) ) {
@@ -175,14 +250,14 @@ var processed_file = async.queue(function (obj, callback) { //todo make better
   }
 
   parser = new Transform({objectMode: true});
-  parser._transform = transform_cb;
+  parser._transform = transform_compiler();
   parser._task = obj.task;
   parser._user_id = obj.task.user_id;
   parser._price_files_id = obj.price_files_id;
 
   stream_db = db_client.query(copyFrom(query));
   stream_db.on("error", function (err) {
-    console.log("# ERROR stream_db", err)
+    console.log("# ERROR stream_db", err);
   });
   stream_db.on("end", function () {
     callback(); //todo check call task.entry.error
@@ -201,6 +276,32 @@ var processed_file = async.queue(function (obj, callback) { //todo make better
     });
 }, 1);
 
+
+/*
+ * PREPROCESSED FILE
+ */
+var db_preparation = function(user_id, path, file_name, cb) {
+  var to_price_files = [
+    user_id,
+    path + "/" + file_name,
+    "" + file_name,
+    "Обработка завершена",
+    1,
+    {"goods_quality": "1", "delivery_time": "1", "discount": "0"},
+    3 //todo maybe to config
+  ];
+  db_client.query('INSERT INTO price_files (user__id, path, name, status, active, info, price_type__id) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING id', to_price_files, function (err, res) {
+    if ( err ) return cb( err );
+    if ( !res.rows[0].id ) return cb("### ERROR on get price_files_id");
+
+    db_client.query('DELETE FROM prices_wholesale WHERE user__id=$1', [user_id], function (err) {
+      if ( err ) return cb( err );
+      cb( err, res.rows[0].id );
+    });
+  });
+
+};
+
 var preprocessed_file = async.queue(function ( obj, callback ) {
   if ( /\.(zip)$/i.test( obj.file_name ) ) {
     fs.createReadStream( config.temp_folder + obj.file_name )
@@ -214,7 +315,7 @@ var preprocessed_file = async.queue(function ( obj, callback ) {
           obj.entry = entry;
           path = obj.task.host + "/" + obj.task.path + "/" + file_name;
           db_preparation( task.user_id, path, file_name, function(err, price_files_id){
-            if ( err ) return console.log(err);
+            if ( err ) return console.log( err );
             console.log('price_files_id', file_name, price_files_id);
             obj.price_files_id = price_files_id;
             processed_file.push( obj );
@@ -233,7 +334,7 @@ var preprocessed_file = async.queue(function ( obj, callback ) {
       obj.entry = fs.createReadStream( config.temp_folder + obj.file_name );
       var path = obj.task.host + "/" + obj.task.path + "/" + obj.file_name;
       db_preparation( task.user_id, path, obj.file_name, function(err, price_files_id){
-        if ( err ) return console.log(err);
+        if ( err ) return console.log( err );
         console.log('price_files_id', obj.file_name, price_files_id);
         obj.price_files_id = price_files_id;
         processed_file.push( obj );
@@ -243,32 +344,14 @@ var preprocessed_file = async.queue(function ( obj, callback ) {
   }
 }, 1);
 
+/*
+ * SWITCH TASK and TASK TYPE PROCESSED
+ */
+
 var check_file_to_download = function( task, file_name ){
   if ( ~task.file_list.indexOf( file_name ) ) return true;
   //todo check task.file_mask and task.file_extension_to_download
   return false;
-};
-
-var db_preparation = function(user_id, path, file_name, cb) {
-  var to_price_files = [
-    user_id,
-    path + "/" + file_name,
-    "" + file_name,
-    "Обработка завершена",
-    1,
-    {"goods_quality": "1", "delivery_time": "1", "discount": "0"},
-    3 //todo maybe to config
-  ];
-  db_client.query('INSERT INTO price_files (user__id, path, name, status, active, info, price_type__id) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING id', to_price_files, function (err, res) {
-    if (err) return cb(err);
-    if (!res.rows[0].id) return cb("### ERROR on get price_files_id");
-
-    db_client.query('DELETE FROM prices_wholesale WHERE user__id=$1', [user_id], function (err) {
-      if (err) return cb(err);
-      cb(err, res.rows[0].id);
-    });
-  });
-
 };
 
 var ftp_processed = function( task ) {
@@ -291,7 +374,7 @@ var ftp_processed = function( task ) {
         cb();
       });
     }, function ( err ) {
-      if ( err ) return console.log(err);
+      if ( err ) return console.log( err );
       console.log('all files were downloaded');
     });
   });
@@ -316,39 +399,53 @@ var task_processed = function(task, cb) {
   })(task, cb);
 };
 
-var parse_intv = setInterval(function () { //todo check changes config.check_interval
-  if (parse_cycle_active) return console.error("Not finished prev cycle!");
+/*
+ * MAIN INTERVAL
+ */
+var parse_cycle = function () {
+  var parse_check_interval = config.check_interval;
+  if ( parse_cycle_active ) return console.error( "Not finished prev cycle!" );
   console.log('# Start parse cycle', new Date());
 
   parse_cycle_active = true; //todo on error set false
   read_config();
-  db_client = new pg.Client(config.db_connection_string);
+  if ( !parse_intv || parse_check_interval !== config.check_interval ) {
+    if ( parse_intv ) clearInterval( parse_intv );
+    parse_intv = setInterval( parse_cycle, config.check_interval );
+  }
+
+  db_client = new pg.Client( config.db_connection_string );
   db_client.connect();
 
-  if (!fs.existsSync(config.temp_folder))
-    fs.mkdirSync(config.temp_folder);
+  if ( !fs.existsSync( config.temp_folder ) )
+    fs.mkdirSync( config.temp_folder );
 
-  async.eachLimit(config.list, 1, function ( task, cb ) {
+  async.eachLimit( config.list, 1, function ( task, cb ) {
     console.log("start task", task.name, new Date());
-    task_processed(task, cb);
+    task_processed( task, cb );
   }, function ( err ) {
     if ( err ) return console.log(err);
     console.log('# All task were finished', new Date());
     db_client.end();
+    //todo clear temp folder
+    //todo close all connection
     parse_cycle_active = false;
   });
-}, config.check_interval);
+};
+
 
 /*
- - each on config.list, use async
- -- connect to file storage
- -- get file list and filtering from config
- -- check file changed
- -- insert to price_files and delete old rows on table to insert(prices_wholesale)
- -- processing file(unzip if archive), check file type from config
- -- transform from config options
- - close all connection
-
+ ,
+ {
+ "name": "test_1 91.197.10.216",
+ "type": "http",
+ "host": "http://91.197.10.216",
+ "path": "/files/",
+ "file_selector": "a[href^='/files/']",
+ "file_extension": ["zip", "csv", "xls"],
+ "file_list": [],
+ "file_mask": [""]
+ }
 
  # DB structure
  id | code | relevant_code | manufacturer | name | count | price | price_files_id | user_id | fts | delivere
